e37178c5e10158a5dab75e8424e74d9ce08e8b36
hclIES <hugo.cayonlaso@iesmiguelherrero.com>
2023-06-09 15:04:40 +0200

    Calculadora Hugo Cayon Laso

---
+++ ejemplo/src/main/java/ed/ejemplo/Ejemplo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ed.ejemplo;
+
+/**
+ *
+ * @author DAW102
+ */
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Esto es una prueba");
+    }
+}
